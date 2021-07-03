ee57385d6c9a2764b9c9f9c65af63bf6494d7fe3
alpha <alphaqiu@gmail.com>
2021-07-03 13:28:07 +0800

    submit v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/p1024k/hello
+module github.com/p1024k/hello/v2
 
 go 1.16
--- hello.go
+++ hello.go
@@ -4,4 +4,9 @@ import "fmt"
 
 func Say() {
 	fmt.Println("Hello World!")
+	fmt.Println("世界你好！")
+}
+
+func SayLegacy() {
+	fmt.Println("Hello World!")
 }
